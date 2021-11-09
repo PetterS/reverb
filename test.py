0ad6d23925d69e15aa1350b575d09dcc367193f9
Petter Strandmark <petter.strandmark@gmail.com>
2021-11-09 08:51:34 +0100

    Record while playing sound

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
+from os import stat
 from numpy.fft import fftfreq
 import matplotlib.pyplot as plt
 import numpy as np
 import sounddevice
+import sys
 
 def main():
     high = 4000
@@ -52,4 +54,62 @@ def main2():
         print('#' * 80)
         input()
 
-main2()
+
+def main3():
+    device = None
+    amplitude = 0.2
+    frequency = 440
+    window_size = 0.05
+    high = 4000
+    low = 50
+    fs = 44100
+
+    state = 0
+    start_idx = 0
+    collected_time = []
+    collected_magnitude = []
+    def callback(indata, outdata, frames, time, status):
+        nonlocal start_idx, state, collected_time, collected_magnitude
+        if status:
+            print(status, file=sys.stderr)
+
+
+        print(time.currentTime)
+
+        fft = np.fft.rfft(indata[:, 0])
+        freq = np.fft.fftfreq(indata.shape[0], d=1.0/fs)
+        # Because we use rfft
+        freq = freq[:fft.shape[0] - 1]
+        fft = fft[:fft.shape[0] - 1]
+        ind = np.logical_and(low <= freq, freq <= high)
+        freq = freq[ind]
+        fft = fft[ind]
+
+        collected_time.append(time.currentTime)
+        db = 10.0 * np.log10(np.abs(fft))
+        collected_magnitude.append(np.interp(frequency, freq, db))
+
+        if state == 0:
+            outdata[:] = 0
+        elif state == 1:
+            t = (start_idx + np.arange(frames)) / fs
+            t = t.reshape(-1, 1)
+            outdata[:] = amplitude * np.sin(2 * np.pi * frequency * t)
+            start_idx += frames
+
+    with sounddevice.Stream(device=device, blocksize=int(window_size * fs), samplerate=fs, channels=(1, 1), callback=callback):
+        state = 0
+        print("Recording silence...")
+        sounddevice.sleep(1000)
+        print("Recording tone")
+        state = 1
+        sounddevice.sleep(1000)
+
+    for i in range(len(collected_time) - 1, -1, -1):
+        collected_time[i] -= collected_time[0]
+
+    plt.plot(collected_time, collected_magnitude)
+    plt.show()
+
+
+main3()
