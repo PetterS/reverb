48a0ea759555c8163dd3caacc9f216cd49814214
Petter Strandmark <petter.strandmark@gmail.com>
2021-11-09 12:45:02 +0100

    Plot the results of several measurements

--- reverb.py
+++ reverb.py
@@ -1,7 +1,7 @@
 import sys
+from typing import Union
 
 from dataclasses import dataclass, field
-from matplotlib.colors import from_levels_and_colors
 import numpy as np
 import sounddevice
 
@@ -9,9 +9,9 @@ import sounddevice
 @dataclass
 class Measurement:
     frequency: float
-    time: list[float] = field(default_factory=lambda: [])
-    magnitude: list[float] = field(default_factory=lambda: [])
-    state: list[int] = field(default_factory=lambda: [])
+    time: Union[np.array, list[float]] = field(default_factory=lambda: [])
+    magnitude: Union[np.array, list[float]] = field(default_factory=lambda: [])
+    state: Union[np.array, list[int]] = field(default_factory=lambda: [])
 
 
 def collect_frequency(
@@ -67,7 +67,7 @@ def collect_frequency(
         print("Recording silence...")
         sounddevice.sleep(int(interval_time * 1000))
         state = 1
-        print("Recording tone...")
+        print(f"Recording tone of {frequency:.0f} Hz...")
         sounddevice.sleep(int(interval_time * 1000))
         state = 2
         print("Recording silence...")
@@ -76,4 +76,65 @@ def collect_frequency(
     for i in range(len(result.time) - 1, -1, -1):
         result.time[i] -= result.time[0]
 
+    result.time = np.array(result.time)
+    result.magnitude = np.array(result.magnitude)
+    result.state = np.array(result.state, dtype=np.int32)
     return result
+
+
+def analyze(measurement: Measurement, ax=None):
+    label = f"{measurement.frequency} Hz"
+
+    robust_max = np.partition(measurement.magnitude, -2)[-2]
+
+    max_ind = np.nonzero(measurement.magnitude >= robust_max - 1.5)[0]
+    max_level = np.median(measurement.magnitude[max_ind])
+
+    i = max_ind[0] - 2
+    min_level = np.max(measurement.magnitude[range(0, i)])
+
+    start_decline_i = max_ind[-1]
+    start_decline_t = measurement.time[start_decline_i]
+
+    last_big_i = np.nonzero(measurement.magnitude > min_level + 5.0)[0][-1]
+    last_big_t = measurement.time[last_big_i]
+    end_decline_i = np.nonzero(
+        np.logical_and(
+            measurement.time > last_big_t, measurement.magnitude < min_level + 1.0
+        )
+    )[0][0]
+    end_decline_t = measurement.time[end_decline_i]
+
+    reverb_time = end_decline_t - start_decline_t
+
+    if ax:
+        p = ax.plot(measurement.time, measurement.magnitude, ".-", label=label)
+        ax.plot(
+            measurement.time[max_ind],
+            measurement.magnitude[max_ind],
+            "o",
+            color=p[0].get_color(),
+        )
+        ax.plot(
+            [measurement.time[0], measurement.time[-1]],
+            [max_level, max_level],
+            ":",
+            color="k",
+        )
+        ax.plot(
+            [measurement.time[0], measurement.time[-1]],
+            [min_level, min_level],
+            ":",
+            color="k",
+        )
+        yl = ax.get_ylim()
+        ax.plot([start_decline_t, start_decline_t], yl, "-", color="k")
+        ax.plot([end_decline_t, end_decline_t], yl, "-", color="k")
+
+        ax.set_xlabel("Time (s)")
+        ax.set_ylabel("Magnitude (dB)")
+        ax.set_title(
+            f"Response for {measurement.frequency:.0f} Hz. t={reverb_time:.2f} s"
+        )
+
+    return reverb_time
--- test.py
+++ test.py
@@ -1,18 +1,40 @@
+import pickle
+import glob
+import math
+
 import matplotlib.pyplot as plt
 
 import reverb
 
 
 def main3():
-    freq = 440
-    result = reverb.collect_frequency(freq)
+    base = 440
+    freqs = [0.5 * base, 1.0 / math.sqrt(2) * base, base, math.sqrt(2) * base, 2 * base]
+    results = []
 
-    plt.plot(result.time, result.magnitude, "x-")
-    plt.xlabel("Time (s)")
-    plt.ylabel("Magnitude (dB)")
-    plt.title(f"Response for {freq} Hz")
+    for f in freqs:
+        filename = f"freq-{f}.pickle"
+        try:
+            result = pickle.load(open(filename, "rb"))
+        except FileNotFoundError:
+            result = reverb.collect_frequency(f)
+            pickle.dump(result, open(filename, "wb"))
+        results.append(result)
 
-    plt.plot(result.time, result.state)
+    plt.rcParams.update({"font.size": 8})
+    _, ax = plt.subplots(2, 4, figsize=(14, 7))
+    ax = ax.reshape(-1)
+
+    data_x = []
+    data_y = []
+    for i, result in enumerate(results):
+        t = reverb.analyze(result, ax=ax[i])
+        data_x.append(f"{result.frequency:.0f}")
+        data_y.append(t)
+
+    ax[-1].bar(data_x, data_y)
+    ax[-1].set_xlabel("Frequency (Hz)")
+    ax[-1].set_ylabel("Reverb time")
     plt.show()
 
 
